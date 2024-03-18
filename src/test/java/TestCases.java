582e73612cda7eeb87ef3b3b60543b475b462988
Amr Hegazy <68474553+Amr-Hegazy1@users.noreply.github.com>
2024-03-18 22:02:33 +0200

    added cleanup to test cases

---
+++ src/test/java/TestCases.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+
+import org.junit.Test;
+
+public class TestCases {
+
+    
+
+    public void cleanUp() throws IOException{
+
+        // delete tables directory
+
+        String tablesPath = "tables/";
+
+        
+
+        Path dir = Paths.get(tablesPath); //path to the directory  
+        Files
+            .walk(dir) // Traverse the file tree in depth-first order
+            .sorted(Comparator.reverseOrder())
+            .forEach(path -> {
+                try {
+                    System.out.println("Deleting: " + path);
+                    Files.delete(path);  //delete each file or directory
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+
+    }
+
+}
